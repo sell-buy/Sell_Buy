d9c5f65b297be1917906fa7b138369716ffe1290
htryu55 <htryu55@gmail.com>
2024-12-23 15:43:48 +0900

    test

--- src/main/java/com/sell_buy/sell_buy/controller/MainController.java
+++ src/main/java/com/sell_buy/sell_buy/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String test() {
+        System.out.println("Hello World");
         return "index";
     }
 }
