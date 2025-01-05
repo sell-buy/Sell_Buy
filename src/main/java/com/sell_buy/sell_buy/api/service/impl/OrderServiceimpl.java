5368a0b753ae9f413c1229eb39975a01fa1a2441
lowshot31 <lowshot31@gmail.com>
2025-01-06 00:29:57 +0900

    ORDER테스트 코드 작성 완료

--- src/main/java/com/sell_buy/sell_buy/api/service/impl/OrderServiceimpl.java
+++ src/main/java/com/sell_buy/sell_buy/api/service/impl/OrderServiceimpl.java
@@ -1,10 +1,18 @@
 package com.sell_buy.sell_buy.api.service.impl;
 
 import com.sell_buy.sell_buy.api.service.OrderService;
+import com.sell_buy.sell_buy.db.entity.Order;
+import com.sell_buy.sell_buy.db.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceimpl  implements OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderServiceimpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     public void getSellOrder(long seller_id) {
     }
@@ -18,5 +26,14 @@ public class OrderServiceimpl  implements OrderService {
 
     }
 
+    @Override
+    public void setOrderId(long order_id) {
+
+    }
+
+    @Override
+    public Order registerOrder(Order order) {
+        return orderRepository.save(order);
+    }
 
 }
--- src/main/java/com/sell_buy/sell_buy/db/repository/MemberRepository.java
+++ src/main/java/com/sell_buy/sell_buy/db/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByUserId(Long userId);
+//    Member findByUserId(Long userId);
 }
