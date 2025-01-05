a1cb5babbb98475313edd3155793a9a1454fdae0
lowshot31 <lowshot31@gmail.com>
2025-01-06 00:30:41 +0900

    ORDER테스트 코드 작성 완료

--- src/main/java/com/sell_buy/sell_buy/api/controller/OrderController.java
+++ src/main/java/com/sell_buy/sell_buy/api/controller/OrderController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
