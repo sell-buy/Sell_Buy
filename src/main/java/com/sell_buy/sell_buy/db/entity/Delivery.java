cd8c9111e56f90f602c1dc049292e9e4b9845efd
htryu55 <htryu55@gmail.com>
2025-01-20 11:24:56 +0900

    Revert "Merge remote-tracking branch 'origin/prod/prod-add-Feature' into prod/prod-add-Feature-alt"
    This reverts commit 239d9886baecbc42fc721859d342b638766eef28, reversing
    changes made to 8b21ee60acdc08ff31809fcbd630b5637dbf1edf.

---
+++ src/main/java/com/sell_buy/sell_buy/db/entity/Delivery.java
@@ -0,0 +1,26 @@
+package com.sell_buy.sell_buy.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter //setter 이용시 인스턴스값이 어디서 꼬일지 모름
+@Builder // 객체만들어서 생성  OPEN -> BUILDER() , CLOSE -> BUILD()
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "DELIVERY")
+public class Delivery {
+    @Id
+    @Column(name = "ORDER_ID")
+    private int orderId;
+    @Column(name = "CARRIER")
+    private String carrier;
+    @Column(name = "TRACKING_NO")
+    private String trackingNo;
+    @Column(name = "CARRIER_ID")
+    private String carrierId;
+}
